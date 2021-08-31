23e6b0ca17d93cc39be1cbd0e8c0910e1945aac2
Aaron Wong <aaronwlw@gmail.com>
2021-08-31 12:23:47 +1000

    created mentorships controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'seeding db'
+
+User.create(first_name: "Anja", last_name: "Drayton", email: "anja@email.com", password: "secret")
+User.create(first_name: "Aaron", last_name: "Wong", email: "aaron@email.com", password: "secret")
+User.create(first_name: "Chad", last_name: "Franceschini", email: "chaddy@email.com", password: "secret")
+User.create(first_name: "George", last_name: "Kettle", email: "george@email.com", password: "secret")
+Mentorship.create(user_id: 2, subject: 'test subject', description: 'come be my test subject')
+Mentorship.create(user_id: 3, subject: 'maths', description: 'im pretty good at maths')
+MentorshipApplication.create(user_id: 1, mentorship_id: 1)
+MentorshipApplication.create(user_id: 3, mentorship_id: 1)
+MentorshipApplication.create(user_id: 4, mentorship_id: 2)
+
+puts 'seeding db done'
+# Aaron is Chad and Anja's mentor
+# Chad is George's mentor
