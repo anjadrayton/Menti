6974000e92cd58708657130ca52bd08f2441a2fa
Aaron Wong <aaronwlw@gmail.com>
2021-08-31 11:19:29 +1000

    created controller skeleton

---
+++ app/controllers/mentorship_applications_controller.rb
@@ -0,0 +1,2 @@
+class MentorshipApplicationsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :mentorships do
+    resources :mentorship_applications
+  end
+
+  get 'dashboard', to: 'dashboard#dashboard'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
