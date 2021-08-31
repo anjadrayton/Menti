8954cbdb5125bf999edf22a1b14403a8996a6e72
Aaron Wong <aaronwlw@gmail.com>
2021-08-31 17:02:23 +1000

    create dashboard UI

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,7 @@
 class DashboardController < ApplicationController
+  def dashboard
+    @mentorships = Mentorship.where(user: current_user)
+    @created_applications = MentorshipApplication.where(user: current_user)
+    @received_applications  = MentorshipApplication.where(mentorship: @mentorships)
+  end
 end
--- app/controllers/mentorships_controller.rb
+++ app/controllers/mentorships_controller.rb
@@ -1,6 +1,5 @@
 class MentorshipsController < ApplicationController
   def index
-    # Im going to gather all the existing mentorships so I can give them to the view to display (by storing the mentorships in an instance variable)
     @mentorships = Mentorship.all
   end
 
@@ -14,9 +13,8 @@ class MentorshipsController < ApplicationController
 
   def create
     @mentorship = Mentorship.new(mentorship_params)
-    @user = User.find(1)
+    @user = User.find(current_user)
     @mentorship.user = @user
-    # raise
     if @mentorship.save
       redirect_to mentorship_path(@mentorship)
     else
