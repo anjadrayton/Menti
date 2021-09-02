c605e6e5f12ce73406276d1100c2e7215c70b1e0
Aaron Wong <aaronwlw@gmail.com>
2021-09-02 15:32:51 +1000

    created search and mapbox

--- app/models/mentorship.rb
+++ app/models/mentorship.rb
@@ -2,4 +2,14 @@ class Mentorship < ApplicationRecord
   belongs_to :user
   has_many :mentorship_application, dependent: :destroy
   has_one_attached :photo
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
+  include PgSearch::Model
+  pg_search_scope :search_by_subject,
+    against: [ :subject ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_021329) do
+ActiveRecord::Schema.define(version: 2021_09_02_024438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,9 @@ ActiveRecord::Schema.define(version: 2021_09_02_021329) do
     t.string "subject"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_mentorships_on_user_id"
   end
 
